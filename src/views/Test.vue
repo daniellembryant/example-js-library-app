e70d9a104bddccf56d043577e1ae6cea2ede8c5f
daniellembryant <danbry08@yahoo.com>
2021-10-14 18:30:05 -0500

    Adds example-js-library homework

---
+++ src/views/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="test">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import Darkmode from "darkmode-js";
+
+new Darkmode().showWidget();
+
+export default {
+  data: function () {
+    return {
+      message: "This is a test page",
+    };
+  },
+
+  methods: {},
+};
+</script>
